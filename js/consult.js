0e54c200d8ad7cbcdfeeb53dd758f9ad992400f2
Jeff Milton <jeff.milton@gmail.com>
2017-11-11 20:39:56 -0500

    -- %1

--- js/consult.js
+++ js/consult.js
@@ -17,9 +17,9 @@ $(document).ready(function () {
         $(function () {
             consolelog("in function");
             /* this URL gets all teh time/place data and returns a JSON string */
-            var astroPosAPI = "http://slider.com/charting/home/online_calcs/scripts/right_now_JSON.php?session_name=right_now_JSON";
+            var astroPosAPI = "/charting/home/online_calcs/scripts/right_now_JSON.php?session_name=right_now_JSON";
             consolelog(astroPosAPI);
-            var getWheelAPI = "http://slider.com/charting/home/online_calcs/scripts/right_now_wheel_JSON.php?";
+            var getWheelAPI = "/charting/home/online_calcs/scripts/right_now_wheel_JSON.php?";
             consolelog(getWheelAPI);
             try {
                 consolelog("x1");
